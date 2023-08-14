e612e71ed5ca6cc1f8cc256a16b9cea987120041
agkyawpai <aungkyawpaing65@gmail.com>
2023-08-14 10:18:20 +0630

    add letter count function

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -3,8 +3,13 @@ import { useState } from "react";
 function AddNote(props) {
     const { handleAddNote } = props;
     const [noteText, setNoteText] = useState("");
+    const [count, setCount] = useState(200);
     const handleChange = (e) => {
-        setNoteText(e.target.value);
+        const text = e.target.value;
+        if(text.length<=200) {
+        setNoteText(text);
+        setCount(Math.max(200 - text.length, 0));
+        }
     }
     const handleSaveClick = () => {
         if(noteText.trim().length > 0) {
@@ -20,7 +25,7 @@ function AddNote(props) {
         <div className="note new">
             <textarea value={noteText} rows="8" cols="10" placeholder="Type to add notes..." onChange={handleChange}></textarea>
             <div className="note-footer">
-                <small>200 Remaining</small>
+                <small>{count} Remaining</small>
                 <button className="save" onClick={handleSaveClick}>Save</button>
             </div>
         </div>
